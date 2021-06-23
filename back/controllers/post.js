47fa7d9790c951dd0e53f50e58a77f98efa8f2ee
o2caledonie <krn.sigura@yahoo.com>
2021-06-23 18:36:55 +0200

    CTRL updatePost

--- back/controllers/post.js
+++ back/controllers/post.js
@@ -28,50 +28,88 @@ exports.createPost = (req, res, next) => {
 }
 
 exports.updatePost = (req, res, next) => {
-    console.log(req.user);//Connected User
-    const id = req.post.id;
-    console.log(id);
     const content = req.body.content;
     const postObject = req.file ?
         {
             content: req.body.content,
             image: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`
         } : { ...req.body }
-   
+
     db.Post.findOne({
-        where: { id: id },
+        where: { id: req.params.id },
     })
-        .then((user) => {
-            if (user) {
-                db.Post.update({ ...postObject, id: id }, { where: { id: id } })
-                    .then(() => res.status(200).json({ ...userPost, message: 'Votre publication a bien été modifiée !' }))
+        .then((post) => {
+            console.log(req.user.id)
+            console.log(post.ownerId)
+            if (post) {
+                if (req.user.id != post.ownerId && req.user.isAdmin === false) {
+                    return res.status(403).json({ error: 'Vous n\'êtes pas autorisé à effectuer cette action' })
+                }
+                db.Post.update(postObject, { where: { id: req.params.id } })
+                    .then(() => res.status(200).json({ message: 'Votre publication a bien été modifiée !' }))
                     .catch(error => res.status(400).json({ error: 'Une erreur s\'est produite !' }))
             }
             else {
-                res.status(404).json({ error: 'Utilisateur non trouvé' });
+                res.status(404).json({ error: 'Publication non trouvée' });
             }
         })
         .catch((error) => {
             console.log(error)
             res.status(500).json({ error: 'Une erreur s\'est produite !' })
         });
+}
 
-    // Content field must be completed
-    if (req.user.id != id && req.user.isAdmin === false) {
-        return res.status(403).json({ error: 'Vous n\'êtes pas autorisé à effectuer cette action' })
-    }
-    const post = db.Post.build({
-        content: req.body.content,
-        image: req.file ? `${req.protocol}://${req.get('host')}/images/${req.file.filename}` : req.body.image,
-        ownerId: id
+exports.deletePost = (req, res, next) => {
+    const content = req.body.content;
+    const postObject = req.file ?
+        {
+            content: req.body.content,
+            image: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`
+        } : { ...req.body }
+
+    db.Post.findOne({
+        where: { id: req.params.id },
     })
-    post.save()
-        .then(() => {
-            res.status(201).json({ message: 'Votre message a bien été créé !' })
+        .then((post) => {
+            console.log(req.user.id)
+            console.log(post.ownerId)
+            if (post) {
+                if (req.user.id != post.ownerId && req.user.isAdmin === false) {
+                    return res.status(403).json({ error: 'Vous n\'êtes pas autorisé à effectuer cette action' })
+                }
+                db.Post.update(postObject, { where: { id: req.params.id } })
+                    .then(() => res.status(200).json({ message: 'Votre publication a bien été modifiée !' }))
+                    .catch(error => res.status(400).json({ error: 'Une erreur s\'est produite !' }))
+            }
+            else {
+                res.status(404).json({ error: 'Publication non trouvée' });
+            }
         })
         .catch((error) => {
             console.log(error)
-            res.status(400).json({ error: 'Une erreur s\'est produite !' })
+            res.status(500).json({ error: 'Une erreur s\'est produite !' })
+        });
+}
+
+exports.getOnePost = (req, res, next) => {
+    db.Post.findOne({
+        where: { id: req.params.id },
+        include: [{
+            model: db.User,
+            as: 'owner',
+            attributes: ['id', 'userName', 'avatar']
+        }]
+    })
+        .then((post) => {
+            if (post) {
+                res.status(200).json(post);
+            } else {
+                res.status(404).json({ error: 'Aucun message trouvé' });
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+            res.status(500).send({ error: 'Une erreur s\'est produite !' });
         });
 }
 
@@ -97,25 +135,5 @@ exports.getAllPosts = (req, res, next) => {
         });
 }
 
-exports.getOnePost = (req, res, next) => {
-    db.Post.findOne({
-        where: { id: req.params.id },
-        include: [{
-            model: db.User,
-            as: 'owner',
-            attributes: ['id', 'userName', 'avatar']
-        }]
-    })
-        .then((post) => {
-            if (post) {
-                res.status(200).json(post);
-            } else {
-                res.status(404).json({ error: 'Aucun message trouvé' });
-            }
-        })
-        .catch((error) => {
-            console.log(error)
-            res.status(500).send({ error: 'Une erreur s\'est produite !' });
-        });
-}
+
 
